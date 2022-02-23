67014ef618fac44f81cb213b46ac4fb0832b8e95
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-23 20:52:12 -0300

    progress from previous commit

--- nft-generator.sh
+++ nft-generator.sh
@@ -17,27 +17,41 @@ num_eyes=${#eye[*]}
 #echo -n "${eye[$((RANDOM%num_eyes))]} of "
 #echo ${background[$((RANDOM%num_backgrounds))]}
 
-find_duplicate="${background[$((RANDOM%num_backgrounds))]} ${eye[$((RANDOM%num_eyes))]}"
-
 nftno="1"
 
-if [ -f nfts/test/2.txt ]; then
-   for filename in nfts/test/*.txt; do
-      if [ "$(cat $filename)" = "$find_duplicate" ]; then
-         echo "Duplicate reached! Skipping..."
-      else
-         echo "${background[$((RANDOM%num_backgrounds))]}" >> nfts/test/$nftno.txt
-         echo "${eye[$((RANDOM%num_eyes))]}" >> nfts/test/$nftno.txt
-         echo "Background: ${background[$((RANDOM%num_backgrounds))]}"
-         echo "Eye: ${eye[$((RANDOM%num_eyes))]}"
-         nftno="$(($nftno + 1))"
+mkdir nfts/test
+
+if [ ! -f nfts/test/1.txt ]; then
+   background_result="${background[$((RANDOM%num_backgrounds))]}"
+   eye_result="${eye[$((RANDOM%num_eyes))]}"
+   echo "$background_result $eye_result" >> nfts/test/$nftno.txt
+   #echo "$eye_result" >> nfts/test/$nftno.txt
+   echo "Background: $background_result"
+   echo "Eye: $eye_result"
+   find_duplicate="$background_result $eye_result"
+   nftno="$(($nftno + 1))"
+fi
+
+if [ -f nfts/test/1.txt ]; then
+   while true; do
+      for filename in nfts/test/*.txt; do
+         background_result="${background[$((RANDOM%num_backgrounds))]}"
+         eye_result="${eye[$((RANDOM%num_eyes))]}"
+         find_duplicate="$background_result $eye_result"
+         if [ "$(cat $filename)" = "$find_duplicate" ]; then
+            echo "Duplicate reached! Skipping..."
+         else
+            #background_result="${background[$((RANDOM%num_backgrounds))]}"
+            #eye_result="${eye[$((RANDOM%num_eyes))]}"
+            echo "$background_result $eye_result" >> nfts/test/$nftno.txt
+            #echo "${eye[$((RANDOM%num_eyes))]}" >> nfts/test/$nftno.txt
+            echo "Background: $background_result"
+            echo "Eye: $eye_result"
+            #find_duplicate="$background_result $eye_result"
+            nftno="$(($nftno + 1))"
 fi
 done
-else
-   echo "${background[$((RANDOM%num_backgrounds))]}" >> nfts/test/1.txt
-   echo "${eye[$((RANDOM%num_eyes))]}" >> nfts/test/1.txt
-   echo "Background: ${background[$((RANDOM%num_backgrounds))]}"
-   echo "Eye: ${eye[$((RANDOM%num_eyes))]}"
+done
 fi
 
 exit 0
