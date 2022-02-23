7ce6441dbe2a215de490095ed6b0982f83d9e186
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-23 20:32:04 -0300

    start trying to implement matching to avoid duplicates

--- nft-generator.sh
+++ nft-generator.sh
@@ -17,8 +17,28 @@ num_eyes=${#eye[*]}
 #echo -n "${eye[$((RANDOM%num_eyes))]} of "
 #echo ${background[$((RANDOM%num_backgrounds))]}
 
-echo "Background: ${background[$((RANDOM%num_backgrounds))]}"
-echo "Eye: ${eye[$((RANDOM%num_eyes))]}"
+find_duplicate="${background[$((RANDOM%num_backgrounds))]} ${eye[$((RANDOM%num_eyes))]}"
+
+nftno="1"
+
+if [ -f nfts/test/2.txt ]; then
+   for filename in nfts/test/*.txt; do
+      if [ "$(cat $filename)" = "$find_duplicate" ]; then
+         echo "Duplicate reached! Skipping..."
+      else
+         echo "${background[$((RANDOM%num_backgrounds))]}" >> nfts/test/$nftno.txt
+         echo "${eye[$((RANDOM%num_eyes))]}" >> nfts/test/$nftno.txt
+         echo "Background: ${background[$((RANDOM%num_backgrounds))]}"
+         echo "Eye: ${eye[$((RANDOM%num_eyes))]}"
+         nftno="$(($nftno + 1))"
+fi
+done
+else
+   echo "${background[$((RANDOM%num_backgrounds))]}" >> nfts/test/1.txt
+   echo "${eye[$((RANDOM%num_eyes))]}" >> nfts/test/1.txt
+   echo "Background: ${background[$((RANDOM%num_backgrounds))]}"
+   echo "Eye: ${eye[$((RANDOM%num_eyes))]}"
+fi
 
 exit 0
 
