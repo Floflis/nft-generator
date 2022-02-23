c1911ba8c4f09929110bfa7166425d1f03077e9e
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-23 19:47:54 -0300

    read attributes from external file

--- pick-card.sh
+++ pick-card.sh
@@ -3,30 +3,10 @@
 
 # This is an example of choosing random elements of an array.
 
+. ./load
 
-# Pick a card, any card.
-
-Suites="Clubs
-Diamonds
-Hearts
-Spades"
-
-Denominations="2
-3
-4
-5
-6
-7
-8
-9
-10
-Jack
-Queen
-King
-Ace"
 
 # Note variables spread over multiple lines.
-
 
 suite=($Suites)                # Read into array variable.
 denomination=($Denominations)
@@ -37,12 +17,6 @@ num_denominations=${#denomination[*]}
 echo -n "${denomination[$((RANDOM%num_denominations))]} of "
 echo ${suite[$((RANDOM%num_suites))]}
 
-
-# $bozo sh pick-cards.sh
-# Jack of Clubs
-
-
-# Thank you, "jipe," for pointing out this use of $RANDOM.
 exit 0
 
 #- from: https://tldp.org/LDP/abs/html/randomvar.html
