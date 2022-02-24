eb7de924f922a9289a07fc9f724dc72266e55792
Daniella Mesquita <daniellmesquita@protonmail.com>
2022-02-23 21:23:34 -0300

    complement previous commit

--- nft-generator.sh
+++ nft-generator.sh
@@ -25,34 +25,67 @@ if [ ! -f nfts/test/1.txt ]; then
    background_result="${background[$((RANDOM%num_backgrounds))]}"
    eye_result="${eye[$((RANDOM%num_eyes))]}"
    echo "$background_result $eye_result" >> nfts/test/$nftno.txt
-   #echo "$eye_result" >> nfts/test/$nftno.txt
    echo "Background: $background_result"
    echo "Eye: $eye_result"
    find_duplicate="$background_result $eye_result"
    nftno="$(($nftno + 1))"
 fi
 
+generate () {
+background_result="${background[$((RANDOM%num_backgrounds))]}"
+eye_result="${eye[$((RANDOM%num_eyes))]}"
+find_duplicate="$background_result $eye_result"
+}
+
+proceed () {
+echo "$background_result $eye_result" >> nfts/test/$nftno.txt
+echo "Background: $background_result"
+echo "Eye: $eye_result"
+nftno="$(($nftno + 1))"
+}
+
+duplicate_check_loop () {
+generate
+for filename in nfts/test/*.txt; do
+   if [ "$(cat $filename)" = "$find_duplicate" ]; then
+      echo "Duplicate reached! Skipping..."
+   else
+      proceed
+fi
+done
+}
+
+#if [ -f nfts/test/1.txt ]; then
+#   
+#fi
+
+#while true; do
+#   
+#done
+
 if [ -f nfts/test/1.txt ]; then
    while true; do
-      for filename in nfts/test/*.txt; do
-         background_result="${background[$((RANDOM%num_backgrounds))]}"
-         eye_result="${eye[$((RANDOM%num_eyes))]}"
-         find_duplicate="$background_result $eye_result"
-         if [ "$(cat $filename)" = "$find_duplicate" ]; then
-            echo "Duplicate reached! Skipping..."
-         else
-            #background_result="${background[$((RANDOM%num_backgrounds))]}"
-            #eye_result="${eye[$((RANDOM%num_eyes))]}"
-            echo "$background_result $eye_result" >> nfts/test/$nftno.txt
-            #echo "${eye[$((RANDOM%num_eyes))]}" >> nfts/test/$nftno.txt
-            echo "Background: $background_result"
-            echo "Eye: $eye_result"
-            #find_duplicate="$background_result $eye_result"
-            nftno="$(($nftno + 1))"
-fi
-done
+      duplicate_check_loop
 done
 fi
+
+#if [ -f nfts/test/1.txt ]; then
+#   while true; do
+#      for filename in nfts/test/*.txt; do
+#         background_result="${background[$((RANDOM%num_backgrounds))]}"
+#         eye_result="${eye[$((RANDOM%num_eyes))]}"
+#         find_duplicate="$background_result $eye_result"
+#         if [ "$(cat $filename)" = "$find_duplicate" ]; then
+#            echo "Duplicate reached! Skipping..."
+#         else
+#            echo "$background_result $eye_result" >> nfts/test/$nftno.txt
+#            echo "Background: $background_result"
+#            echo "Eye: $eye_result"
+#            nftno="$(($nftno + 1))"
+#fi
+#done
+#done
+#fi
 
 exit 0
 
